52ecb2381d919e5e91b5017e66ccba4bf5b4e68d
Rupalishanay <rupalad14@gmail.com>
2025-11-11 12:51:42 -0500

    updated explicit-and-implicit-conversion-in-javascript

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -21,12 +21,26 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 let result = "5" - 2;
 console.log("The result is: " + result);
+//Javascript converts String("5") into numbers before perform the operation
 
 let isValid = Boolean("false");
 if (isValid) {
     console.log("This is valid!");
 }
+//Output : This is valid!
+
+let isInvalid = Boolean("");
+if (isInvalid){
+  console.log("This is invalid!");
+}
+//Values like  0 ,  null ,  undefined ,  NaN , and  ""  (empty string) are falsy (convert to  false ), while everything else is truthy (convert to  true ).
 
 let age = "25";
-let totalAge = age + 5;
+//let totalAge = age + 5;
+//console.log("Total Age: " + totalAge);
+//"+" operator perform string concatination in above example 5 is converted to "5" and concatinate with "25"
+//Output : 255
+
+let totalAge=Number(age) + 5;
 console.log("Total Age: " + totalAge);
+//"25" String is converted into Number using Number() then perform the Addition operation.
